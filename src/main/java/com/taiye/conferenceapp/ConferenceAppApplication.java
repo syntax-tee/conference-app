2c535c0763eed0006e5489d0a71980561da04c3d
taiyeogunlade <ogunladetaiye@gmail.com>
2020-04-29 19:20:00 +0100

    -initial commit

--- src/main/java/com/taiye/conferenceapp/ConferenceAppApplication.java
+++ src/main/java/com/taiye/conferenceapp/ConferenceAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConferenceAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConferenceAppApplication.class, args);
     }
